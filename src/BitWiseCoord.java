a21502f17779ceb8710db2dfe384a243cebf4109
Niklas Lundström <luni14@nti.umea>
2016-11-25 11:36:33 +0100

    Initial commit

---
+++ src/BitWiseCoord.java
@@ -0,0 +1,28 @@
+/**
+ * Created by luni14 on 2016-11-25.
+ */
+public class BitWiseCoord {
+    private short Coords;
+
+    //konstruktor
+    public BitWiseCoord(int x, int y){
+        Coords = 0;
+        setHigh(x);
+        setLow(y);
+    }
+    public void setHigh(int x) {
+
+    }
+
+    public void setLow(int y) {
+
+    }
+
+    public void getHigh() {
+
+    }
+
+    public void getLow() {
+
+    }
+}
